fd21b9d8dbd1e69d7e1385e3516d47c947e8b141
tharindudg <tdgalappaththi@gmail.com>
2017-10-15 13:23:39 +0700

    making Fragments explicit

--- src/main/scala/io/github/pauljamescleary/petstore/repository/DoobieOrderRepositoryInterpreter.scala
+++ src/main/scala/io/github/pauljamescleary/petstore/repository/DoobieOrderRepositoryInterpreter.scala
@@ -1,30 +1,20 @@
 package io.github.pauljamescleary.petstore.repository
 
-import doobie._
-import doobie.implicits._
 import cats._
 import cats.implicits._
+import doobie._
+import doobie.implicits._
 import io.github.pauljamescleary.petstore.model.{Order, OrderStatus}
 import org.joda.time.DateTime
 
-class DoobieOrderRepositoryInterpreter[F[_]: Monad](val xa: Transactor[F])
-    extends OrderRepositoryAlgebra[F] {
+class DoobieOrderRepositoryInterpreter[F[_] : Monad](val xa: Transactor[F])
+  extends OrderRepositoryAlgebra[F] {
 
   // This will clear the database.  Note, this would typically be done via something like FLYWAY (TODO)
-  private val dropOrdersTable = sql"""
-    DROP TABLE IF EXISTS ORDERS
-  """.update.run.transact(xa)
+  private val dropOrdersTable = dropOrdersDBTable(xa)
 
   // The tags column is controversial, could be a lookup table.  For our purposes, indexing on tags to allow searching is fine
-  private val createOrdersTable = sql"""
-    CREATE TABLE ORDERS (
-      ID   SERIAL,
-      PET_ID INT8 NOT NULL,
-      SHIP_DATE TIMESTAMP NULL,
-      STATUS VARCHAR NOT NULL,
-      COMPLETE BOOLEAN NOT NULL
-    )
-  """.update.run.transact(xa)
+  private val createOrdersTable = createOrdersDBTable(xa)
 
   /* We require type StatusMeta to handle our ADT Status */
   private implicit val StatusMeta: Meta[OrderStatus] =
@@ -37,37 +27,67 @@ class DoobieOrderRepositoryInterpreter[F[_]: Monad](val xa: Transactor[F])
       dt => new java.sql.Timestamp(dt.getMillis)
     )
 
-  def migrate: F[Int] =
+  def migrate: F[Int] = {
     dropOrdersTable >> createOrdersTable
+  }
 
   def put(order: Order): F[Order] = {
+    val f: Fragment = sql"REPLACE INTO ORDERS (PET_ID, SHIP_DATE, STATUS, COMPLETE) values (${order.petId}, ${order.shipDate}, ${order.status}, ${order.complete})"
     val insert: ConnectionIO[Order] =
       for {
-        id <- sql"REPLACE INTO ORDERS (PET_ID, SHIP_DATE, STATUS, COMPLETE) values (${order.petId}, ${order.shipDate}, ${order.status}, ${order.complete})".update
-          .withUniqueGeneratedKeys[Long]("ID")
+        id <- f.update.withUniqueGeneratedKeys[Long]("ID")
       } yield order.copy(id = Some(id))
     insert.transact(xa)
   }
 
-  def get(orderId: Long): F[Option[Order]] =
-    sql"""
+  def get(orderId: Long): F[Option[Order]] = {
+    val f: Fragment =
+      sql"""
       SELECT PET_ID, SHIP_DATE, STATUS, COMPLETE
         FROM ORDERS
        WHERE ID = $orderId
-     """.query[Order].option.transact(xa)
+     """
+    f.query[Order].option.transact(xa)
+  }
 
-  def delete(orderId: Long): F[Option[Order]] =
+  def delete(orderId: Long): F[Option[Order]] = {
     get(orderId).flatMap {
       case Some(order) =>
-        sql"DELETE FROM ORDERS WHERE ID = $orderId".update.run
+        val f: Fragment = sql"DELETE FROM ORDERS WHERE ID = $orderId"
+        f.update.run
           .transact(xa)
           .map(_ => Some(order))
       case None =>
         none[Order].pure[F]
     }
+  }
+
+  private def dropOrdersDBTable(xa: doobie.Transactor[F]) = {
+    val f: Fragment =
+      sql"""
+    DROP TABLE IF EXISTS ORDERS
+  """
+    f.update.run.transact(xa)
+  }
+
+  private def createOrdersDBTable(xa: doobie.Transactor[F]) = {
+    val f: Fragment =
+      sql"""
+    CREATE TABLE ORDERS (
+      ID   SERIAL,
+      PET_ID INT8 NOT NULL,
+      SHIP_DATE TIMESTAMP NULL,
+      STATUS VARCHAR NOT NULL,
+      COMPLETE BOOLEAN NOT NULL
+    )
+  """
+    f.update.run.transact(xa)
+  }
 }
 
 object DoobieOrderRepositoryInterpreter {
-  def apply[F[_]: Monad](xa: Transactor[F]): DoobieOrderRepositoryInterpreter[F] =
+  def apply[F[_] : Monad](
+                           xa: Transactor[F]): DoobieOrderRepositoryInterpreter[F] = {
     new DoobieOrderRepositoryInterpreter(xa)
+  }
 }
